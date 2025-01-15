ca47d9cb471f2c8193687d0b47c45f92231d0a0b
Joseph <underradicals@gmail.com>
2025-01-15 17:17:14 -0600

    Injestion Phase

---
+++ main.py
@@ -0,0 +1,146 @@
+﻿import json
+from requests import Session
+from zipfile import ZipFile
+from manifest_models import Manifest
+from settings import (
+    SETTINGS_PATH,
+    MANIFEST_PATH,
+    ENVIRONMENT,
+    LANGUAGE,
+    BASE_ADDRESS,
+    WORLD_DATA,
+    WORLD_CONTENT_ZIP,
+    WORLD_CONTENT_DB
+)
+
+
+def get_db_url(manifest: Manifest, lang: str) -> str:
+    match lang:
+        case "en":
+            return manifest.Response.mobile_world_content_paths.en
+        case "fr":
+            return manifest.Response.mobile_world_content_paths.fr
+        case "es":
+            return manifest.Response.mobile_world_content_paths.es
+        case "es-mx":
+            return manifest.Response.mobile_world_content_paths.esmx
+        case "de":
+            return manifest.Response.mobile_world_content_paths.de
+        case "it":
+            return manifest.Response.mobile_world_content_paths.it
+        case "ja":
+            return manifest.Response.mobile_world_content_paths.ja
+        case "pt-br":
+            return manifest.Response.mobile_world_content_paths.ptbr
+        case "ru":
+            return manifest.Response.mobile_world_content_paths.ru
+        case "pl":
+            return manifest.Response.mobile_world_content_paths.pl
+        case "ko":
+            return manifest.Response.mobile_world_content_paths.ko
+        case "zh-cht":
+            return manifest.Response.mobile_world_content_paths.zhcht
+        case "zh-chs":
+            return manifest.Response.mobile_world_content_paths.zhchs
+        case _:
+            return manifest.Response.mobile_world_content_paths.en
+
+
+def get_json_urls(manifest: Manifest, lang: str) -> list:
+    match lang:
+        case "en":
+            return [(f'{x}.json', y) for x, y in
+                    manifest.Response.json_world_component_content_paths.en.to_dict().items()]
+        case "fr":
+            return [(f'{x}.json', y) for x, y in
+                    manifest.Response.json_world_component_content_paths.fr.to_dict().items()]
+        case "es":
+            return [(f'{x}.json', y) for x, y in
+                    manifest.Response.json_world_component_content_paths.es.to_dict().items()]
+        case "es-mx":
+            return [(f'{x}.json', y) for x, y in
+                    manifest.Response.json_world_component_content_paths.esmx.to_dict().items()]
+        case "de":
+            return [(f'{x}.json', y) for x, y in
+                    manifest.Response.json_world_component_content_paths.de.to_dict().items()]
+        case "it":
+            return [(f'{x}.json', y) for x, y in
+                    manifest.Response.json_world_component_content_paths.it.to_dict().items()]
+        case "ja":
+            return [(f'{x}.json', y) for x, y in
+                    manifest.Response.json_world_component_content_paths.ja.to_dict().items()]
+        case "pt-br":
+            return [(f'{x}.json', y) for x, y in
+                    manifest.Response.json_world_component_content_paths.ptbr.to_dict().items()]
+        case "ru":
+            return [(f'{x}.json', y) for x, y in
+                    manifest.Response.json_world_component_content_paths.ru.to_dict().items()]
+        case "pl":
+            return [(f'{x}.json', y) for x, y in
+                    manifest.Response.json_world_component_content_paths.pl.to_dict().items()]
+        case "ko":
+            return [(f'{x}.json', y) for x, y in
+                    manifest.Response.json_world_component_content_paths.ko.to_dict().items()]
+        case "zh-cht":
+            return [(f'{x}.json', y) for x, y in
+                    manifest.Response.json_world_component_content_paths.zhcht.to_dict().items()]
+        case "zh-chs":
+            return [(f'{x}.json', y) for x, y in
+                    manifest.Response.json_world_component_content_paths.zhchs.to_dict().items()]
+        case _:
+            return [(f'{x}.json', y) for x, y in
+                    manifest.Response.json_world_component_content_paths.en.to_dict().items()]
+
+
+def download_d2_json(s: Session, iterable: list, dev: bool = False):
+    r = s.get(f'{BASE_ADDRESS}{iterable[1]}')
+    r.raise_for_status()
+    p = WORLD_DATA.joinpath(iterable[0])
+    if not dev:
+        if str(p).endswith("zip"):
+            p.write_bytes(r.content)
+        else:
+            p.write_text(r.text, encoding='utf-8')
+        print(f'{iterable[1]} downloaded to {p.name}')
+    elif dev and p.exists():
+        print(f'{p.name} already exists')
+        return
+    else:
+        if str(p).endswith("zip"):
+            p.write_bytes(r.content)
+        else:
+            p.write_text(r.text, encoding='utf-8')
+        print(f'{iterable[1]} downloaded to {p.name}')
+
+
+WORLD_DATA.mkdir(parents=True, exist_ok=True)
+MANIFEST_PATH.touch(exist_ok=True)
+settings_content = SETTINGS_PATH.read_text(encoding='utf-8')
+settings_json = json.loads(settings_content)
+http_session = Session()
+if ENVIRONMENT != 'DEVELOPMENT':
+    http_session.headers.update(
+        {'Content-Type': 'application/json', 'Accept': 'application/json', 'Accept-Charset': 'UTF-8',
+         'Accept-Encoding': 'gzip, deflate', 'X-Api-Key': settings_json['apikey']})
+    response = http_session.get(f"{BASE_ADDRESS}/platform/destiny2/manifest")
+    response.raise_for_status()
+    manifest_json = response.json()
+    MANIFEST_PATH.write_text(json.dumps(manifest_json, indent=2, ensure_ascii=False))
+else:
+    manifest_text = MANIFEST_PATH.read_text(encoding='utf-8')
+    manifest_json = json.loads(manifest_text)
+    manifest = Manifest(manifest_json)
+    db_url = get_db_url(manifest, LANGUAGE)
+    json_urls = get_json_urls(manifest, LANGUAGE)
+    json_urls.append(["world_content.zip", db_url])
+    for item in json_urls:
+        download_d2_json(http_session, item, dev=True)
+    zip64 = ZipFile(WORLD_CONTENT_ZIP)
+    archive_item_name = zip64.namelist()[0]
+    archive_content = zip64.open(archive_item_name, "r")
+    content = archive_content.read()
+    WORLD_CONTENT_DB.write_bytes(content)
+http_session.close()
+
+if __name__ == '__main__':
+    pass
---
+++ settings/__init__.py
@@ -0,0 +1,14 @@
+﻿from pathlib import Path
+
+ROOT = Path.cwd()
+SETTINGS_PATH = ROOT / 'settings.json'
+MANIFEST_PATH = ROOT / 'manifest.json'
+ENVIRONMENT = "DEVELOPMENT"
+LANGUAGE = "en"
+BASE_ADDRESS = 'https://www.bungie.net'
+
+DATA_DIR = ROOT / 'data'
+SQL_DIR = DATA_DIR / 'sql'
+WORLD_DATA = DATA_DIR / 'world_content_data'
+WORLD_CONTENT_ZIP = WORLD_DATA / 'world_content.zip'
+WORLD_CONTENT_DB = ROOT / 'world_content.db'
