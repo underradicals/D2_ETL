7ac98a7b6b43a28479b64ad2f1ba86e4768ffe01
Joseph <underradicals@gmail.com>
2025-01-21 12:01:31 -0600

    Add AmmoTypeEntityConfiguration

---
+++ D2ETL.Infrastructure/EntityConfigurations/AmmoTypeEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using D2ETL.Core.AmmoTypeDefinition;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace D2ETL.Infrastructure.EntityConfigurations;
+
+public class AmmoTypeEntityConfiguration : IEntityTypeConfiguration<AmmoType>
+{
+    public void Configure(EntityTypeBuilder<AmmoType> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedNever().IsRequired();
+        builder.Property(x => x.Name)
+            .HasColumnType("text")
+            .IsRequired();
+        builder.Property(x => x.Description)
+            .HasColumnType("text")
+            .IsRequired();
+        builder.Property(x => x.Icon)
+            .HasColumnType("text")
+            .IsRequired();
+    }
+}
