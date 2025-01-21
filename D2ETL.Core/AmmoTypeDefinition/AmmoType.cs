8fca545d04ed7760e5bc45ab480e55b22a52f18b
Joseph <underradicals@gmail.com>
2025-01-21 11:56:49 -0600

    Add ammo type entity

---
+++ D2ETL.Core/AmmoTypeDefinition/AmmoType.cs
@@ -0,0 +1,28 @@
+﻿namespace D2ETL.Core.AmmoTypeDefinition;
+
+public class AmmoType
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Icon { get; set; } = string.Empty;
+
+    public AmmoType()
+    {
+        
+    }
+
+    public AmmoType(int id, string name, string description, string icon)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Icon = icon;
+    }
+
+
+    public static AmmoType From(int id, string name, string description, string icon)
+    {
+        return new AmmoType(id, name, description, icon);
+    }
+}
